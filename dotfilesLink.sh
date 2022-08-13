3e9f25d7af3607edb25f574ef8ee4b8d44397f2a
Dotinkasra <tyekepon@pm.me>
2022-08-14 03:30:22 +0900

    スクリプトの些細なミスを修正

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -e curl -fLo "~/.vim/pack/jetpack/opt/vim-jetpack/plugin/jetpack.vim" ];then
+if [ ! -e curl -fLo "~/.vim/pack/jetpack/opt/vim-jetpack/plugin/jetpack.vim" ];then
     curl -fLo ~/.vim/pack/jetpack/opt/vim-jetpack/plugin/jetpack.vim --create-dirs https://raw.githubusercontent.com/tani/vim-jetpack/master/plugin/jetpack.vim
 fi
 ln -sf ~/dotfile/.vimrc ~/.vimrc
